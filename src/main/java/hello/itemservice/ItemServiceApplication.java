ea8aa4bb20ce63f064582eb39a5323ebe3b85271
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-29 23:31:29 +0900

    테스트 dataSource 정보 지우면 Spring이 임베디드 DB 생성함

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -30,17 +30,17 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-	@Bean
-	@Profile("test")
-	public DataSource dataSource() {
-		log.info("메모리 데이터베이스 초기화");
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.h2.Driver");
-		//DB_CLOSE_DELAY=-1(커넥션이 모두 끊기면 데이터베이스도 종료되는데 그것을 방지하는 설정
-		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-		dataSource.setUsername("sa");
-		dataSource.setPassword("");
-		return dataSource;
-	}
+//	@Bean
+//	@Profile("test")
+//	public DataSource dataSource() {
+//		log.info("메모리 데이터베이스 초기화");
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName("org.h2.Driver");
+//		//DB_CLOSE_DELAY=-1(커넥션이 모두 끊기면 데이터베이스도 종료되는데 그것을 방지하는 설정
+//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+//		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+//		return dataSource;
+//	}
 
 }
