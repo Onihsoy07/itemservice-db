8c65e50ec8cf3c16dc08cdeaee5e70ab204d0c9d
Onihsoy07 <84126411+Onihsoy07@users.noreply.github.com>
2023-07-31 22:31:51 +0900

    SpringDataRepository 인터페이스 구현(메서드 추가)

---
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,21 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNameLike(String itemName);
+    List<Item> findByPriceLike(Integer price);
+
+    //쿼리 메서드(아래랑 같은 기능 수행)
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    //쿼리 직접 수행(JPQL)
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+}
